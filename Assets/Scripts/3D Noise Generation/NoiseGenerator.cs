e2c7f2c3bca2c60036b4af4c178ca26a1019f981
VFS\creynolds <pg20curtis@vfs.com>
2022-02-08 11:03:36 -0800

    Basic tiling

--- Assets/Scripts/3D Noise Generation/NoiseGenerator.cs
+++ Assets/Scripts/3D Noise Generation/NoiseGenerator.cs
@@ -11,12 +11,15 @@ public class NoiseGenerator: MonoBehaviour
 
 
     [SerializeField, Range(2,512)] private int _textureResolution;
-    [SerializeField, Range(1, 64)] private int _numberOfPoints;
+    [SerializeField, Range(1, 64)] private int _cellSize;
 
     [SerializeField] private FilterMode _filterMode;
     private PointData[,] _textureData;
 
     private Vector2[] _pointArray;
+    private Vector2[] _leftPointArray;
+
+    private Vector2[] _pointArrayAll;
 
     private Texture2D _texture;
 
@@ -52,8 +55,9 @@ public class NoiseGenerator: MonoBehaviour
 
         //init texture Array
         _textureData = new PointData[_textureResolution, _textureResolution];
-        _pointArray = new Vector2[16];
+        _pointArray = new Vector2[(_textureResolution / _cellSize) * (_textureResolution / _cellSize)];
         GenerateCells();
+        GenerateTilingPoints();
         FillTexture();
     }
 
@@ -65,41 +69,25 @@ public class NoiseGenerator: MonoBehaviour
         if (_texture.width != _textureResolution) _texture.Resize(_textureResolution, _textureResolution);
         if (_texture.filterMode != _filterMode) _texture.filterMode = _filterMode;
 
-        Vector3 point00 = transform.TransformPoint(new Vector3(-0.5f, -0.5f));
-        Vector3 point10 = transform.TransformPoint(new Vector3(0.5f, -0.5f));
-        Vector3 point01 = transform.TransformPoint(new Vector3(-0.5f, 0.5f));
-        Vector3 point11 = transform.TransformPoint(new Vector3(0.5f, 0.5f));
-
-
         float stepSize = 1f / _textureResolution;
         for (int y = 0; y < _textureResolution; y++)
         {
-            //Vector3 point0 = Vector3.Lerp(point00, point01, (y + 0.5f) * stepSize);
-            //Vector3 point1 = Vector3.Lerp(point10, point11, (y + 0.5f) * stepSize);
             for (int x = 0; x < _textureResolution; x++)
             {
-                //Vector3 point = Vector3.Lerp(point0, point1, (x + 0.5f) * stepSize);
                 if(_textureData[x,y].isPoint)
                 {
                     _texture.SetPixel(x, y, Color.red);
                     continue;
                 }
                 _texture.SetPixel(x,y,ColorPixel(FindNearest(new Vector2(x,y))));
-
             }
-            //_texture.SetPixel(0, 0, Color.black);
-            //_texture.SetPixel(15, 0, Color.black);
-            //_texture.SetPixel(0, 15, Color.black);
-            //_texture.SetPixel(15, 15, Color.black);
-
-
         }
         _texture.Apply();
     }
 
     private void GenerateCells()
     {
-        int step = _textureResolution / _numberOfPoints;
+        int step = _textureResolution / _cellSize;
 
         int count = 0;
 
@@ -107,8 +95,8 @@ public class NoiseGenerator: MonoBehaviour
         {
             for (int z = 0; z < step; z++)
             {
-                int x = Random.Range(_numberOfPoints + (_numberOfPoints * i -1), (_numberOfPoints * i));
-                int y = Random.Range(_numberOfPoints + (_numberOfPoints * z - 1), (_numberOfPoints * z));
+                int x = Random.Range(_cellSize + (_cellSize * i -1), (_cellSize * i));
+                int y = Random.Range(_cellSize + (_cellSize * z - 1), (_cellSize * z));
 
                 _textureData[x, y].isPoint = true;
                 _pointArray[count] = new Vector2(x, y);
@@ -117,12 +105,48 @@ public class NoiseGenerator: MonoBehaviour
         }
     }
 
+    private void GenerateTilingPoints()
+    {
+        _pointArrayAll = new Vector2[_pointArray.Length * 5];
+
+        Vector2 leftOffset = new Vector2( 0 - _textureResolution, 0);
+        Vector2 rightOffset = new Vector2(0 + _textureResolution, 0);
+
+        Vector2 topOffset = new Vector2( 0 , 0 + _textureResolution);
+        Vector2 bottomOffset = new Vector2( 0, 0 - _textureResolution);
+
+        int counter = 0;
+
+        for (int i = 0; i < _pointArray.Length; i++)
+        {
+            //Generate center point
+            _pointArrayAll[counter] = _pointArray[i];
+            //Generate left point
+            _pointArrayAll[counter + 1] = _pointArray[i] + leftOffset;
+            //Generate right point
+            _pointArrayAll[counter + 2] = _pointArray[i] + rightOffset;
+            ////Generate top point
+            _pointArrayAll[counter + 3] = _pointArray[i] + topOffset;
+            ////Generate bottom point
+            _pointArrayAll[counter + 4] = _pointArray[i] + bottomOffset;
+            counter += 5;
+        }
+
+
+        for (int i = 0; i < _pointArrayAll.Length; i++)
+        {
+            Debug.Log(_pointArrayAll[i]);
+        }
+
+
+    }
+
     private float FindNearest(Vector2 pos)
     {
         Vector2 shortestPos = Vector2.zero;
         float shortestDistance = float.MaxValue;
         float distance = 0;
-        foreach (Vector2 point in _pointArray)
+        foreach (Vector2 point in _pointArrayAll)
         {
             distance = Vector2.Distance(pos / _textureResolution, point / _textureResolution);
             if (distance < shortestDistance)
@@ -133,7 +157,7 @@ public class NoiseGenerator: MonoBehaviour
         }
         Vector2 newPos = new Vector2((pos.x - _textureResolution/2 ) / _textureResolution, (pos.y - _textureResolution / 2 ) / _textureResolution);
         Vector2 newShortestPos = new Vector2((shortestPos.x - _textureResolution / 2) / _textureResolution, (shortestPos.y - _textureResolution / 2) / _textureResolution);
-        //Debug.DrawLine(newPos, newShortestPos, Color.white, 10f);
+        Debug.DrawLine(newPos, newShortestPos, Color.white, 10f);
         return shortestDistance;
     }
 
@@ -145,9 +169,6 @@ public class NoiseGenerator: MonoBehaviour
 
         float clampedDist = Mathf.Clamp(dist, min, max);
         float value = (clampedDist - min) / (max - min);
-        Debug.Log($"Initial Distanct {dist}, normalized disntance {value}");
         return new Color(value, value, value);
     }
 }
-
-//Range of .2 -> .8s
